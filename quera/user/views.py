516e28c3a217df5508e91bf9c989ecc5c79bbd29
rozhrafiee <rozhinarafiee014@gmail.com>
2024-08-25 19:35:34 +0430

    views of the user app

--- quera/user/views.py
+++ quera/user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Teacher, Student
 
-# Create your views here.
+def teacher_list(request):
+    teachers = Teacher.objects.all()
+    data = []
+    for teacher in teachers:
+        data.append({
+            'name' : teacher.name, 
+            'last_name' : teacher.last_name,
+            'email' : teacher.email,
+            'classes' : list(teacher.classes.values('name')),
+            'bootcamps' : list(teacher.bootcamps.values('name'))
+        })
+    return JsonResponse(data, safe=False)
+
+def student_list(request):
+    students = Student.objects.all()
+    data = []
+    for student in students:
+        data.append({
+            'name' : student.name,
+            'last_name' : student.last_name,
+            'email' : student.email,
+            'classes' : list(student.classes.values('name')),
+            'bootcamps' : list(student.bootcamps.values('name'))
+        })
+    return JsonResponse(data, safe=False)
+
+def techer_detail():
+    pass
