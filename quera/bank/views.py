282127f505789fc0b67f89d2c9978d7198672f02
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-25 20:24:23 +0330

    views pt1 completed

--- quera/bank/views.py
+++ quera/bank/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from bank.models import Question , Lesson
+from learning.models import Class , Bootcamp
+from user.models import Teacher, Student, Student_answer
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+
+def questions_list_(request):
+    questions = Question.objects.all()
+    questions_list = []
+    for question in questions:
+        questions_list.append({
+            "name": question.name,
+            "form": question.form,
+            "answer": question.answer,
+            "publish_date": question.publish_date,
+            "level": question.level,
+        })
+    return JsonResponse(questions_list, safe=False)
+
+def lessons_list_(request):
+    lessons = Lesson.objects.all()
+    lessons_list = []
+    for lesson in lessons:
+        lessons_list.append({
+            'subject': lesson.subject,
+            'text': lesson.text,
+        }
+        )
+    return JsonResponse(lessons_list, safe=False)
+def add_question(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        question_name = data['name']
+        question_form = data['form']
+        question_answer = data['answer']
+        question_publish_date = data['publish_date']
+        question_level = data['level']
+        question = Question.objects.create(name = question_name, form = question_form, answer = question_answer, publish_date = question_publish_date, level = question_level)
+        question.save()
+
+        return JsonResponse({'message': 'Question added successfully'})
+    return JsonResponse({'message': 'Invalid request method'})
+
+def add_lesson(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        lesson_subject = data['subject']
+        lesson_text = data['text']
+        lesson = Lesson.objects.create(subject = lesson_subject, text = lesson_text)
+        lesson.save()
+        return JsonResponse({'message': 'Lesson added successfully'})
+    return JsonResponse({'message': 'Invalid request method'})
+            
--- quera/learning/views.py
+++ quera/learning/views.py
@@ -1,3 +1,105 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from user.models import Teacher, Student, Student_answer
 from django.shortcuts import render
+import json
+from bank.models import Question,  Lesson
+from learning.models import Class ,Bootcamp
 
-# Create your views here.
+def bootcamp_list(request):
+    bootcamps = Bootcamp.objects.all()
+    data = []
+    for bootcamp in bootcamps:
+        data.append({
+            'name' : bootcamp.name,
+            'start_date' : bootcamp.start_date,
+            'price' : bootcamp.price
+        })
+    return JsonResponse(data, safe=False)
+def classes_list(request):
+    classes = Class.objects.all()
+    data = []
+    for class_ in classes:
+        data.append({
+            'name' : class_.name,
+            'assignments' : [assignmet.name for assignmet in class_.assingment.all()],
+            'lessons' : [lesson.subject for lesson in class_.lesson.all()],
+        })
+    return JsonResponse(data, safe=False)
+
+def creat_class(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            class_name = data['class_name']
+
+            teacher_id = data['teacher_id']
+            teacher = Teacher.objects.get(id=teacher_id)
+            teacher : Teacher
+            class_ = Class.objects.create(name=class_name)
+            class_.save()
+            teacher.classes.add(class_)
+            return JsonResponse({'message': 'class created successfully and teacher was assigned to it'})
+        except Class.DoesNotExist:
+            return JsonResponse({'error': 'class not created'})
+        except Teacher.DoesNotExist:
+            return JsonResponse({'error': 'teacher does not exist'})
+    else:
+        return JsonResponse({'error': 'Post request required'}, status=405)
+def add_assignmet_to_class(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            class_id = data['class_id']
+            class_to_add = Class.objects.get(id=class_id)
+            assignmet_id = data['assignmet_id']
+            assignmet_to_add = Lesson.objects.get(id=assignmet_id)
+            class_to_add.assingment.add(assignmet_to_add)
+            class_to_add.save()
+            return JsonResponse({'message': 'assignmet added to class successfully'})
+        except Class.DoesNotExist:
+            return JsonResponse({'error': 'class does not exist'})
+        except Lesson.DoesNotExist:
+            return JsonResponse({'error': 'assignmet does not exist'})
+    else:
+        return JsonResponse({'error': 'Post request required'}, status=405)
+
+def add_lesson_to_class(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            class_id = data['class_id']
+            class_to_add = Class.objects.get(id=class_id)
+            lesson_id = data['lesson_id']
+            lesson_to_add = Lesson.objects.get(id=lesson_id)
+            class_to_add.lesson.add(lesson_to_add)
+            class_to_add.save()
+            return JsonResponse({'message': 'lesson added to class successfully'})
+        except Class.DoesNotExist:
+            return JsonResponse({'error': 'class does not exist'})
+        except Lesson.DoesNotExist:
+            return JsonResponse({'error': 'lesson does not exist'})
+    else:
+        return JsonResponse({'error': 'Post request required'}, status=405)
+
+def add_bootcamp(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            bootcamp_name = data['bootcamp_name']
+            start_date = data['start_date']
+            price = data['price']
+            bootcamp = Bootcamp.objects.create(name=bootcamp_name, start_date=start_date, price=price)
+            teacher_id = data['teacher_id']
+            teacher = Teacher.objects.get(id=teacher_id)
+            teacher : Teacher
+            teacher.bootcamps.add(bootcamp)
+            teacher.save()
+            bootcamp.save()
+            return JsonResponse({'message': 'bootcamp created successfully and teacher was assigned to it'})
+        except Bootcamp.DoesNotExist:
+            return JsonResponse({'error': 'bootcamp not created'})
+        except Teacher.DoesNotExist:
+            return JsonResponse({'error': 'teacher does not exist'})
+    else:
+        return JsonResponse({'error': 'Post request required'}, status=405)
