2c8dbee92883bafe6135035b5b2bcaa700ca6bf0
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-03 19:09:55 +0330

    user login and refresh set

--- quera/user/views.py
+++ quera/user/views.py
@@ -13,9 +13,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
+class Login(TokenObtainPairView):
+    pass
 
-
+class Refresh(TokenRefreshView):
+    pass
 class TeachersListView(ListAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
